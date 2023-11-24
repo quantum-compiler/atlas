178b6566f911d654113c4da6f071974a3550b7de
caoshiyi <shicao@berkeley.edu>
2023-11-24 01:00:55 -0800

    minor

--- scripts/permultter/bench/srun-1-quartz.sh
+++ scripts/permultter/bench/srun-1-quartz.sh
@@ -6,135 +6,28 @@
 #SBATCH -N 1
 #SBATCH --gpus-per-node=4
 
-rm -rf /pscratch/sd/z/zjia/qs/result-srun/torque/*_30.log
-rm -rf /pscratch/sd/z/zjia/qs/result-srun/torque/*_29.log
-rm -rf /pscratch/sd/z/zjia/qs/result-srun/torque/*_28.log
+module load cray-mpich/8.1.25
 module load nccl
 module load cudatoolkit
 conda activate qs
 export PATH=$PATH:/pscratch/sd/z/zjia/qs/torque/deps/quartz/external/HiGHS/build/bin
 export MPICH_GPU_SUPPORT_ENABLED=1
 
+# Define the array of strings
+strings=("ae" "dj" "ghz" "graphstate" "ising" "qft" "qpeexact" "qsvm" "su2random" "vqc" "wstate")
 
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qft --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/qft_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qftentangled --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qftentangled_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ghz --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ghz_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit graphstate --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/graphstate_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit twolocalrandom --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/twolocalrandom_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit realamprandom --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/realamprandom_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit su2random --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/su2random_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ae --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ae_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeexact --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeexact_30.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeinexact --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeinexact_30.log
+# Iterate over the strings
+for str in "${strings[@]}"; do
+    # Execute the command with the current string
+    /pscratch/sd/z/zjia/qs/torque/build2/examples/mpi-based/simulate --import-circuit ${str} --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/${str}_28.log
+done
 
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qft --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qft_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qftentangled --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qftentangled_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ghz --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ghz_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit graphstate --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/graphstate_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit twolocalrandom --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/twolocalrandom_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit realamprandom --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/realamprandom_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit su2random --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/su2random_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ae --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ae_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeexact --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeexact_28.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeinexact --n 28 --local 28 --device 1 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeinexact_28.log
+for str in "${strings[@]}"; do
+    # Execute the command with the current string
+    /pscratch/sd/z/zjia/qs/torque/build2/examples/mpi-based/simulate --import-circuit ${str} --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/${str}_29.log
+done
 
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qft --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qft_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qftentangled --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qftentangled_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ghz --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ghz_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit graphstate --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/graphstate_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit twolocalrandom --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/twolocalrandom_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit realamprandom --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/realamprandom_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit su2random --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/su2random_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit ae --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/ae_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeexact --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeexact_29.log
-srun -u \
-     --ntasks="$(( SLURM_JOB_NUM_NODES ))" \
-     --ntasks-per-node=1\
-     /pscratch/sd/z/zjia/qs/torque/build/examples/mpi-based/simulate --import-circuit qpeinexact --n 29 --local 28 --device 2 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/qpeinexact_29.log
+for str in "${strings[@]}"; do
+    # Execute the command with the current string
+    /pscratch/sd/z/zjia/qs/torque/build2/examples/mpi-based/simulate --import-circuit ${str} --n 30 --local 28 --device 4 --use-ilp > /pscratch/sd/z/zjia/qs/result-srun/torque/${str}_30.log
+done
